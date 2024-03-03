aafbde134f884b4ae27389e61f7854ea9fbb84e7
Ib-patel <patelishan5572gmail.com>
2024-03-03 00:17:27 -0800

    Login Signup complete

---
+++ static/script/script.js
@@ -0,0 +1,17 @@
+console.log("ISHAN PATEL")
+
+document.getElementById("btnpress").addEventListener("click", myFunction);
+
+
+   
+function myFunction() {
+
+    let myUsername = document.getElementsByClassName("form-control-1");
+    let myUseremail = document.getElementById("email");
+    let myUserpassword = document.getElementById("password");
+
+    console.log(myUsername); 
+    
+  
+  }
+  
